39b75c4dabb51b169fd4ef8d1846c692a5b41051
Gaurav Sharma <sharmagaurav459@gmail.com>
2020-03-15 22:06:48 -0700

    Create Inner_Join_2.sql

---
+++ Inner_Join_2.sql
@@ -0,0 +1,11 @@
+SELECT c.code, name, region, e.year, fertility_rate, unemployment_rate
+  -- 1. From countries (alias as c)
+  FROM countries AS c
+  -- 2. Join to populations (as p)
+  INNER JOIN populations AS p
+    -- 3. Match on country code
+    ON c.code = p.country_code
+  -- 4. Join to economies (as e)
+  INNER JOIN economies AS e
+    -- 5. Match on country code and year
+    ON p.country_code = e.code AND p.year = e.year;
